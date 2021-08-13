7fff169bdcd829574ae5a999e325b2b65d4d9be8
AlexisNoeMunoz <87334844+AlexisNoeMunoz@users.noreply.github.com>
2021-08-12 22:14:40 -0600

    Correcion controladores no se refrescan
    Correccion de que los controladores no se refrescaban ni cargando la pagina en docker

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -69,8 +69,8 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-  # config.file_watcher = ActiveSupport::FileUpdateChecker
+  # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.file_watcher = ActiveSupport::FileUpdateChecker
   config.webpacker.check_yarn_integrity = false
 
   # Uncomment if you wish to allow Action Cable access from any origin.
